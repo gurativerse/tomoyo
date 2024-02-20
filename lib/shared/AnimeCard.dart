3df18fbdc23ca5036973baf695760e9d31fabb40
Kanyanut S <kanyanutsompong@gmail.com>
2024-02-20 13:03:46 +0700

    feat: AnimeCard

---
+++ lib/shared/AnimeCard.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import '../theme.dart';
+import '../client/AnimeInfo.dart';
+
+class AnimeCard extends StatelessWidget {
+  final String animeOriginalName;
+  final String animeEngName;
+  final String animePoster;
+  final String availablePlatform;
+
+  const AnimeCard({
+    Key? key,
+    required this.animeOriginalName,
+    required this.animeEngName,
+    required this.animePoster,
+    required this.availablePlatform,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AnimeInfoPage(
+                        animeOriginalName: animeOriginalName,
+                        animeEngName: animeEngName,
+                        animePoster: animePoster,
+                        availablePlatform: availablePlatform,
+                      )));
+        },
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          elevation: 0,
+          color: Colors.transparent,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage(animePoster),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 5,
+                      right: 5,
+                      child: Container(
+                        width: 25,
+                        height: 25,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('./asset/$availablePlatform.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: RichText(
+                  overflow: TextOverflow.ellipsis,
+                  strutStyle: StrutStyle(fontSize: 12.0),
+                  text: TextSpan(
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Color(ColorPalatte.color['shadow']!),
+                      ),
+                      text: animeOriginalName),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: RichText(
+                  overflow: TextOverflow.ellipsis,
+                  strutStyle: StrutStyle(fontSize: 12.0),
+                  text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                      text: animeEngName),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
